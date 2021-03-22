f045da424cdde871724c01d4e6a37ddee0167dbd
stas <stas-723@ya.ru>
2021-03-23 02:48:59 +0300

    homework

---
+++ 1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введи число a:");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введи число:");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int x;
+
+            if (a > b) ;
+            {
+
+                x = a + b;
+
+            }
+
+            if (a == b) ;
+            {
+
+                x = a * b;
+            }
+
+            if (a < b) ;
+            {
+                x = a - b;
+
+            }
+
+            Console.WriteLine($"результат = {x}");
+
+
+
+
+        }
+
+
+
+
+    }
+
+
+
+}
