040398b3212d4edb2e1f0412944fd597d1c6d033
stas <stas-723@ya.ru>
2021-03-23 03:12:11 +0300

    HomeWork3

---
+++ 3/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введи число a:");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введи число b:");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введи число c:");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+
+            if (a <= b && b <= c && a <= c)
+            {
+
+
+                Console.WriteLine($"{a},{b},{c}");
+            }
+
+            else if (a <= b && b >= c && a <= c)
+            {
+
+                Console.WriteLine($"{a},{c},{b}");
+
+            }
+
+            else if (a >= b && b <= c && c <= a)
+            {
+
+                Console.WriteLine($"{b},{c},{a}");
+            }
+
+            else if (a >= b && b <= c && c >= a)
+            {
+
+                Console.WriteLine($"{b},{a},{c}");
+            }
+
+            else if (a >= b && b >= c && c <= a)
+            {
+
+                Console.WriteLine($"{c},{b},{a}");
+            }
+
+            else if (a <= b && b >= c && c <= a)
+            {
+
+                Console.WriteLine($"{c},{a},{b}");
+            }
+
+
+
+        }
+
+
+
+
+    }
+
+
+
+}
