059a116234b0c83bbc074260443a995746331ba0
stas <stas-723@ya.ru>
2021-03-23 03:19:54 +0300

    HomeWork4

---
+++ 4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите значение a");
+            double a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение b");
+            double b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение c");
+            double c = Convert.ToInt32(Console.ReadLine());
+
+            double x = (c - b) / a;
+
+            Console.WriteLine($"Результат: {x}");
+
+
+        }
+    }
+}
